93c6e4c6869e5f1b9045d9d1fb7b04aade5a7a8f
Janny <uspex-uspet@yandex.ru>
2012-06-01 16:21:47 +0300

    User model with passwords, and user show page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,9 +8,26 @@ describe UsersController do
       get 'new'
       response.should be_success
     end
- it "should have the right title" do
+   it "should have the right title" do
       get 'new'
       response.should have_selector("title", :content => "Sign up")
+    end
+  end
+
+  describe "GET 'show'" do
+
+    before(:each) do
+      @user = Factory(:user)
+    end
+
+    it "should be successful" do
+      get :show, :id => @user
+      response.should be_success
+    end
+
+    it "should find the right user" do
+      get :show, :id => @user
+      assigns(:user).should == @user
     end
   end
 
